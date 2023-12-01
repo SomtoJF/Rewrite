e4663f2170a9a35131369a26ab913f5e3b966acb
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-01 20:52:42 +0100

    BugFix: Empty typeDefs cause server to fail on start

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,3 +1,11 @@
-const typeDefs = `#graphql `;
+const typeDefs = `#graphql 
+# Define Your Schema Here
+type Name{
+    name: String
+}
+
+type Query{
+    name: Name
+}`;
 
 export default typeDefs;
