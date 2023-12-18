c84b594758a9958796ee7924363d4657afe129cc
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-18 10:57:20 +0100

    Initial Commit

---
+++ server/dist/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+async function connectToDatabase() {
+    try {
+        console.log("connecting...");
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log("Connection established");
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+export default connectToDatabase;
---
+++ server/dist/schema/schema.js
@@ -0,0 +1,10 @@
+const typeDefs = `#graphql 
+# Replace this with your own type definitions
+type Name{
+    name: String
+}
+
+type Query{
+    name: Name
+}`;
+export default typeDefs;
