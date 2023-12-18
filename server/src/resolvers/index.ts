acebb1f15c457a62b5fc379f1870dffcda8f3696
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-18 12:39:38 +0100

    Write resolvers for read, delete and creating accounts

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,5 +1,29 @@
+import Account from "../models/accounts.js";
+import { CreateAccountResolverArgs } from "./types/account.js";
+
+type GetByIdArgType = {
+	id: string;
+};
+
 const resolvers = {
-	Query: {},
+	Query: {
+		account: async (_: any, args: GetByIdArgType) =>
+			await Account.findById(args.id),
+	},
+	Mutation: {
+		createAccount: async (_: any, args: CreateAccountResolverArgs) => {
+			try {
+				const account = new Account(args.account);
+				console.log(args);
+				const newAccount = await account.save();
+				return newAccount;
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+		deleteAccount: async (_: any, args: GetByIdArgType) =>
+			await Account.findByIdAndDelete(args.id),
+	},
 };
 
 export default resolvers;
