fae4291df186ba88516bc2f12fd5dfc427b59422
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-18 12:40:49 +0100

    Write schema for account and posts as well as mutations for accounts

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -1,11 +1,45 @@
 const typeDefs = `#graphql 
-# Replace this with your own type definitions
-type Name{
-    name: String
+
+type Account{
+    _id: String!
+    firstname: String!
+    lastname: String!
+    profile_picture: String
+    posts: [Post!]
 }
 
+type Post{
+    _id: String!
+    author_id: Account!
+    title: String!
+    description: String!
+    content: String!
+    tags: [String!]
+}
+
+input CreateAccountArgs{
+    _id: String!
+    firstname: String!
+    lastname: String!
+    profile_picture: String
+}
+
+input UpdateAccountArgs{
+    username: String!
+    profile_picture: String
+}
+
+type Mutation{
+    createAccount(account: CreateAccountArgs!): Account
+    updateAccount(id: String!, edits: UpdateAccountArgs): Account
+    deleteAccount(id: String!): Account
+}
+
+
 type Query{
-    name: Name
+    account(id: String!): Account 
+    posts: [Post]
+    post(id:String!): Post
 }`;
 
 export default typeDefs;
