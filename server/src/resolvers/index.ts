c0bb12193d4445c8ec35e2da1ee137a46f328a23
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-19 13:28:55 +0100

    Write resolver for nested article.author query

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -5,6 +5,8 @@ import updateAccount from "./accounts/updateAccount.js";
 import getArticle from "./articles/getOneArticle.js";
 import getArticles from "./articles/getArticles.js";
 import createArticle from "./articles/createArticle.js";
+import { ArticleInterface } from "../models/types/article.js";
+import Account from "../models/accounts.js";
 
 const resolvers = {
 	Query: {
@@ -13,6 +15,10 @@ const resolvers = {
 		articles: getArticles,
 	},
 	Account: {},
+	Article: {
+		author: async (parent: ArticleInterface) =>
+			await Account.findOne({ _id: parent.author_id }),
+	},
 	Mutation: {
 		createAccount: createAccount,
 		updateAccount: updateAccount,
