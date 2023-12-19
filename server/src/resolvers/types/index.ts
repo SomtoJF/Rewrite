11aa20e6ffbde584ec242756c8938317c4526f80
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-19 11:29:52 +0100

    Initial commit

---
+++ server/src/resolvers/types/index.ts
@@ -0,0 +1,12 @@
+import { CreateAccountResolverArgs } from "./account.js";
+import { UpdateAccountResolverArgs } from "./account.js";
+
+type GetByIdArgType = {
+	id: string;
+};
+
+export type {
+	GetByIdArgType,
+	CreateAccountResolverArgs,
+	UpdateAccountResolverArgs,
+};
