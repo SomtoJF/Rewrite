41c413039ff7aa5a2a644f4c51d02d6753004fbe
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-21 15:34:02 +0100

    import styling

--- client/src/features/authentication/components/RightAside.tsx
+++ client/src/features/authentication/components/RightAside.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import useFormName from "../hooks/useFormName";
 import { Link } from "react-router-dom";
-
+import "./RightAside.styles.sass";
 interface RightAsideProps {
 	children: ReactElement[];
 }
@@ -9,7 +9,7 @@ interface RightAsideProps {
 export default function RightAside({ children }: RightAsideProps) {
 	const activeFormName = useFormName((state) => state.activeFormName);
 	return (
-		<aside>
+		<aside id="right-aside">
 			{children}
 			{activeFormName === "login" ? (
 				<p>
