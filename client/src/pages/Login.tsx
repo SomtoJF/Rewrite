60ba5c5bbb52a7d7804cb7ee7ce6478023d9a86c
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-21 13:34:27 +0100

    Set up signup and login auth

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,46 @@
+import { FormEvent, useState } from "react";
+import { useAuth } from "../contexts/authContext";
+
 export default function Login() {
-	return <div>Login</div>;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const { login } = useAuth();
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		await login(email, password);
+	};
+
+	return (
+		<main>
+			<form
+				action=""
+				onSubmit={(e) => {
+					handleSubmit(e);
+				}}
+			>
+				<h2>Login</h2>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="email@email.com"
+					value={email}
+					onChange={(e) => {
+						setEmail(e.target.value);
+					}}
+				/>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					value={password}
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+				/>
+				<button type="submit">submit</button>
+			</form>
+		</main>
+	);
 }
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,3 +1,46 @@
+import { FormEvent, useState } from "react";
+import { useAuth } from "../contexts/authContext";
+
 export default function Signup() {
-	return <div>Signup</div>;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const { signup } = useAuth();
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		await signup(email, password);
+	};
+
+	return (
+		<main>
+			<form
+				action=""
+				onSubmit={(e) => {
+					handleSubmit(e);
+				}}
+			>
+				<h2>Signup</h2>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="email@email.com"
+					value={email}
+					onChange={(e) => {
+						setEmail(e.target.value);
+					}}
+				/>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					value={password}
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+				/>
+				<button type="submit">submit</button>
+			</form>
+		</main>
+	);
 }
