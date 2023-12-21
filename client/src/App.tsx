095d0dcac4c7eebb8d7f2780c7f6bd6cdaf49557
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-21 11:17:58 +0100

    Set up first three client routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
 import "./App.sass";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <Signup />,
+	},
+]);
 
 function App() {
-	return <div>App</div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
