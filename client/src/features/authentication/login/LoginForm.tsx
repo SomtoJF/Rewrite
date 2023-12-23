ddf806949b36314287bfb5300c77464b08133b18
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-23 16:09:23 +0100

    Navigate to home page on login

--- client/src/features/authentication/login/LoginForm.tsx
+++ client/src/features/authentication/login/LoginForm.tsx
@@ -2,18 +2,21 @@ import { FormEvent, useState } from "react";
 import { useAuth } from "../../../contexts/authContext";
 import "./LoginForm.styles.sass";
 import Loader from "../../../components/Loader";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const { login } = useAuth();
 	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 
 	const handleSubmit = async (e: FormEvent) => {
 		try {
 			e.preventDefault();
 			setLoading(true);
 			await login(email, password);
+			navigate("/");
 		} catch (err: any) {
 			throw new Error(err);
 		} finally {
