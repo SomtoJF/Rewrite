8c6fd557a2e2a7333e46368e85c7533cee0aaf9a
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-24 20:13:27 +0100

    Initial commit

---
+++ client/src/Layout.tsx
@@ -0,0 +1,12 @@
+import "./components/Navbar.styles.sass";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar.tsx";
+
+export default function Layout() {
+	return (
+		<div>
+			<Navbar />
+			<Outlet />
+		</div>
+	);
+}
