4b4bf6d7eb284d6d4242707687875414d6fef7b2
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-25 23:04:22 +0100

    Initial commit

---
+++ client/src/components/Navbar/NavDropdown.tsx
@@ -0,0 +1,77 @@
+import type { MenuProps } from "antd";
+import { Button, Dropdown, Space } from "antd";
+import { ReactElement } from "react";
+import { useAuth } from "../../contexts/authContext";
+
+interface NavDropDownProps {
+	children: ReactElement;
+}
+
+const dropdownButtonStyles = {
+	border: "none",
+	backgroundColor: "transparent",
+	width: "100%",
+};
+
+export default function NavDropdown({ children }: NavDropDownProps) {
+	const { logout } = useAuth();
+	const items: MenuProps["items"] = [
+		{
+			key: 0,
+			label: "My Account",
+		},
+		{
+			type: "divider",
+		},
+		{
+			key: "1",
+			label: (
+				<button
+					style={{ ...dropdownButtonStyles, textAlign: "left" }}
+					type="button"
+					onClick={() => {}}
+				>
+					Profile picture
+				</button>
+			),
+		},
+		{
+			key: "2",
+			danger: true,
+			label: (
+				<button
+					type="button"
+					style={{ ...dropdownButtonStyles, color: "red", textAlign: "left" }}
+					onClick={() => {
+						logout();
+					}}
+					onMouseOver={(e) => {
+						e.currentTarget.style.color = "#f4f4f4";
+					}}
+					onMouseLeave={(e) => {
+						e.currentTarget.style.color = "red";
+					}}
+				>
+					Logout
+				</button>
+			),
+		},
+	];
+
+	return (
+		<Space wrap>
+			<Dropdown menu={{ items }} placement="bottom">
+				<Button
+					style={{
+						width: "fit-content",
+						height: "fit-content",
+						background: "none",
+						border: "none",
+					}}
+				>
+					{children}
+				</Button>
+			</Dropdown>
+		</Space>
+	);
+}
