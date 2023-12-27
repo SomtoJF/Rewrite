dc988dd7b489ad8cf04e99c3d4a9fc672fc69b7d
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-27 13:55:20 +0100

    Initial commit

---
+++ client/src/pages/Account.tsx
@@ -0,0 +1,19 @@
+import Hero from "../features/my_account/components/Hero";
+import MyArticles from "../features/my_account/components/MyArticles";
+import useUserData from "../zustand/useUserData";
+import Error from "./Error";
+
+export default function Account() {
+	const userData = useUserData((state) => state.userData);
+	if (!userData) return <Error />;
+	return (
+		<main className="page">
+			<Hero
+				firstname={userData?.account.firstname}
+				lastname={userData?.account.lastname}
+				profile_picture={userData?.account.profile_picture}
+			/>
+			<MyArticles id={userData.account._id} />
+		</main>
+	);
+}
