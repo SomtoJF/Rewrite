630f44aed2bbd37f2081dafec015f48b63abe6c3
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-31 21:28:24 +0100

    Place limit on image size

--- client/src/features/my_account/components/ProfilePictureModal.tsx
+++ client/src/features/my_account/components/ProfilePictureModal.tsx
@@ -54,6 +54,13 @@ const MyModal = ({ open, setOpen, trigger, title }: props) => {
 
 	const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		if (e.target && e.target.files) {
+			if (e.target.files[0].size > 2097152) {
+				enqueueSnackbar("Image too large, max size is 2MB", {
+					variant: "error",
+				});
+				e.target.files = null;
+				throw new Error("File size exceeded limit of 2MB");
+			}
 			setNewDisplayPicture(e.target.files[0]);
 			const imageUrl = URL.createObjectURL(e.target.files[0]);
 			setPreviewImageUrl(imageUrl);
