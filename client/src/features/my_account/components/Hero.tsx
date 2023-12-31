7e83a3b6c63c6d94fe870e4076578c140cc0bcae
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-12-31 11:59:52 +0100

    Develop modal for changing profile picture

--- client/src/features/my_account/components/Hero.tsx
+++ client/src/features/my_account/components/Hero.tsx
@@ -1,5 +1,7 @@
 import { Avatar } from "@mui/material";
 import "./Hero.styles.sass";
+import Modal from "./ProfilePictureModal.tsx";
+import { useState } from "react";
 
 interface HeroProps {
 	firstname: string;
@@ -12,16 +14,28 @@ export default function Hero({
 	lastname,
 	profile_picture,
 }: HeroProps) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	return (
-		<header id="account-hero">
-			<div />
-			<div>
-				<Avatar sx={{ width: 150, height: 150 }} src={profile_picture}>
-					{" "}
-					{`${firstname.charAt(0)} ${lastname.charAt(0)}`}
-				</Avatar>
-				<p>{`${firstname} ${lastname}`}</p>
-			</div>
-		</header>
+		<>
+			<header id="account-hero">
+				<div />
+				<div>
+					<Modal
+						open={isModalOpen}
+						setOpen={setIsModalOpen}
+						title="Upload new profile picture"
+						trigger={
+							<Avatar sx={{ width: 150, height: 150 }} src={profile_picture}>
+								{" "}
+								{`${firstname.charAt(0)} ${lastname.charAt(0)}`}
+							</Avatar>
+						}
+					/>
+
+					<p>{`${firstname} ${lastname}`}</p>
+				</div>
+			</header>
+		</>
 	);
 }
