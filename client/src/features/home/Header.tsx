249c22d88cb3071274c6fb82d501f6a821cc520d
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-02 22:01:45 +0100

    Animate cursor on enter and leave of header

--- client/src/features/home/Header.tsx
+++ client/src/features/home/Header.tsx
@@ -1,8 +1,32 @@
 import "./Header.styles.sass";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
 
 export default function Header() {
+	const headerRef = useRef<HTMLHeadElement>(null);
+	const scaleCursorUp = () => {
+		gsap.to(".circle", {
+			scale: 2.5,
+		});
+	};
+	const scaleCursorDown = () => {
+		gsap.to(".circle", {
+			scale: 1,
+		});
+	};
+
+	useEffect(() => {
+		const header = headerRef.current;
+		header?.addEventListener("mouseenter", scaleCursorUp);
+		header?.addEventListener("mouseleave", scaleCursorDown);
+
+		return () => {
+			header?.removeEventListener("mouseenter", scaleCursorUp);
+			header?.removeEventListener("mouseleave", scaleCursorDown);
+		};
+	}, []);
 	return (
-		<header id="home-header">
+		<header id="home-header" ref={headerRef}>
 			<h1>REWRITE</h1>
 		</header>
 	);
