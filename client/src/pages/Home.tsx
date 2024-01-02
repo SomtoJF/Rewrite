dbf979b38c2872fb85e7821491cf45265e493d5a
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-02 22:02:21 +0100

    Build inversion cursor

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useRef } from "react";
 import Header from "../features/home/header";
+import "../features/home/Home.style.sass";
 
 export default function Home() {
+	const cursorRef = useRef<HTMLDivElement>(null);
+	const delay = 250;
+
+	function getDimensions(e: MouseEvent) {
+		if (cursorRef.current) {
+			cursorRef.current.style.top = `${e.clientY - 5}px`;
+			cursorRef.current.style.left = `${e.clientX - 5}px`;
+		}
+	}
+
+	function throttle(callback: any, limit: number) {
+		let wait = false;
+		return function () {
+			if (!wait) {
+				callback.apply(null, arguments);
+				wait = true;
+				setTimeout(function () {
+					wait = false;
+				}, limit);
+			}
+		};
+	}
+
+	useEffect(() => {
+		window.addEventListener("mousemove", (e) => {
+			throttle(getDimensions(e), delay);
+		});
+		return window.removeEventListener("mousemove", (e) => {
+			throttle(getDimensions(e), delay);
+		});
+	}, []);
+
 	return (
-		<div className="page">
+		<div className="page" id="home-page">
+			<div className="circle" ref={cursorRef} />
 			<Header />
 		</div>
 	);
