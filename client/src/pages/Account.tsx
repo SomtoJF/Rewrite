564dffdec8809fc102288fa2f12c8ece2883d265
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-03 18:05:43 +0100

    Fetch account information from id not logged user

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,19 +1,42 @@
+import { useParams } from "react-router-dom";
 import Hero from "../features/my_account/components/Hero";
 import MyArticles from "../features/my_account/components/MyArticles";
-import useUserData from "../zustand/useUserData";
+import { gql, useQuery } from "@apollo/client";
+import Error from "../components/Error/Error";
+
+const ACCOUNT_QUERY = gql`
+	query AccountQuery($id: String!) {
+		account(id: $id) {
+			firstname
+			lastname
+			profile_picture
+			articles {
+				title
+				description
+				est_read_time
+				tags
+				createdAt
+			}
+		}
+	}
+`;
 
 export default function Account() {
-	const userData = useUserData((state) => state.userData);
-	if (userData) {
+	const { id } = useParams();
+	const { error, loading, data } = useQuery(ACCOUNT_QUERY, {
+		variables: { id: id },
+	});
+	if (!loading) {
 		return (
 			<main className="page">
 				<Hero
-					firstname={userData?.account.firstname}
-					lastname={userData?.account.lastname}
-					profile_picture={userData?.account.profile_picture}
+					firstname={data.account.firstname}
+					lastname={data.account.lastname}
+					profile_picture={data.account.profile_picture}
 				/>
-				<MyArticles id={userData!.account._id} />
+				<MyArticles id={id!} />
 			</main>
 		);
 	}
+	if (error) return <Error />;
 }
