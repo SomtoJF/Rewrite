591233bce40122e15a1a5694afa49f9b48077c23
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-03 17:04:54 +0100

    request articles from backend

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,38 @@
 import { useEffect, useRef } from "react";
 import Header from "../features/home/Header";
 import "../features/home/Home.style.sass";
+import { ApolloError, gql, useQuery } from "@apollo/client";
+import Articles from "../components/Articles/Articles";
+
+const ARTICLES_QUERY = gql`
+	query ArticlesQuery($page: Int!) {
+		articles(page: $page) {
+			_id
+			title
+			description
+			est_read_time
+			tags
+			createdAt
+			author {
+				firstname
+				lastname
+			}
+		}
+	}
+`;
 
 export default function Home() {
 	const cursorRef = useRef<HTMLDivElement>(null);
 	const delay = 250;
+	const { loading, data } = useQuery(ARTICLES_QUERY, {
+		variables: { page: 1 },
+		onError: (error: ApolloError) => {
+			throw new Error(error.message);
+		},
+		onCompleted: (data) => {
+			console.log(data);
+		},
+	});
 
 	function getDimensions(e: MouseEvent) {
 		if (cursorRef.current) {
@@ -39,6 +67,7 @@ export default function Home() {
 		<div className="page" id="home-page">
 			<div className="circle" ref={cursorRef} />
 			<Header />
+			{!loading && <Articles data={data} />}
 		</div>
 	);
 }
