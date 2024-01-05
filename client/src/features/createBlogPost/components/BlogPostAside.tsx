dda9e36b2a46d8f55e74291c68fbc926a652b8c8
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-05 15:54:28 +0100

    make blog post aside a required field

--- client/src/features/createBlogPost/components/BlogPostAside.tsx
+++ client/src/features/createBlogPost/components/BlogPostAside.tsx
@@ -6,21 +6,15 @@ import { useState } from "react";
 
 interface BlogPostAsideProps {
 	setThumbnail: (value: File) => void;
-	imagesUrl: string[];
-	setImagesUrl: (value: string[]) => void;
 }
 
-export default function BlogPostAside({
-	setThumbnail,
-}: // imagesUrl,
-// setImagesUrl,
-BlogPostAsideProps) {
+export default function BlogPostAside({ setThumbnail }: BlogPostAsideProps) {
 	const [previewThumbnailUrl, setPreviewThumbnailUrl] = useState("");
 
 	return (
 		<aside id="blog-post-aside">
 			<section id="featured-image">
-				<h3>Featured Image</h3>
+				<h3>Featured Image*</h3>
 				<Divider />
 				<Image width={200} src={previewThumbnailUrl} />
 				<InputFileUpload
