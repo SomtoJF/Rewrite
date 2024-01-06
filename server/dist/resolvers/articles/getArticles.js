66769ff7abb6046dac0b7dff1d955068a017de16
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-06 16:40:17 +0100

    Sort articles from most recent to least recent

--- server/dist/resolvers/articles/getArticles.js
+++ server/dist/resolvers/articles/getArticles.js
@@ -3,6 +3,7 @@ const getArticles = async (_, args) => {
     if (args.page <= 0)
         throw new Error("Page number must not be less than 1");
     return await Article.find()
+        .sort({ createdAt: "desc" })
         .skip(15 * (args.page - 1))
         .limit(15);
 };
