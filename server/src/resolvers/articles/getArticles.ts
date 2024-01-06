e53ab40158a93a2afcbc9d112d7de6986d0ad8c8
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-06 14:57:32 +0100

    Modify articles resolver to always get the latest posts first

--- server/src/resolvers/articles/getArticles.ts
+++ server/src/resolvers/articles/getArticles.ts
@@ -4,6 +4,7 @@ import { GetArticlesResolverArgs } from "../types";
 const getArticles = async (_: any, args: GetArticlesResolverArgs) => {
 	if (args.page <= 0) throw new Error("Page number must not be less than 1");
 	return await Article.find()
+		.sort({ createdAt: "desc" })
 		.skip(15 * (args.page - 1))
 		.limit(15);
 };
