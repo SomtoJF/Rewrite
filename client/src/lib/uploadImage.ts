e40945c3cd6dba15996f7b95ae99472a67a7b6ef
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-08 14:36:52 +0100

    make max upload file size 2mb

--- client/src/lib/uploadImage.ts
+++ client/src/lib/uploadImage.ts
@@ -2,6 +2,7 @@ import { UploadClient } from "@uploadcare/upload-client";
 
 const client = new UploadClient({
 	publicKey: import.meta.env.VITE_REACT_APP_UPLOADCARE_KEY,
+	maxContentLength: 2097152,
 });
 
 export default async function uploadImage(file: File) {
