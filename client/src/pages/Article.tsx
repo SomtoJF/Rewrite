d32948d0cfab1355a88cccb52ae5790151a2abb3
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-08 17:57:50 +0100

    Pass props to new Header component

--- client/src/pages/Article.tsx
+++ client/src/pages/Article.tsx
@@ -3,7 +3,9 @@ import { useParams } from "react-router-dom";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import { Spin } from "antd";
 import "../features/readArticle/Article.styles.sass";
+import Header from "../features/readArticle/components/Header";
 
 const ARTICLE_QUERY = gql`
 	query ArticleQuery($id: String!) {
@@ -14,19 +16,46 @@ const ARTICLE_QUERY = gql`
 			tags
 			createdAt
 			thumbnail_url
+			est_read_time
+			author {
+				_id
+				firstname
+				lastname
+				profile_picture
+			}
 		}
 	}
 `;
 
 export default function Article() {
 	const { id } = useParams();
-	const { data, loading } = useQuery(ARTICLE_QUERY, { variables: { id: id } });
+	const { data, loading } = useQuery(ARTICLE_QUERY, {
+		variables: { id: id },
+		onCompleted(data) {
+			console.log(data);
+		},
+	});
 	return (
 		<main id="article-page">
-			{!loading && (
-				<Markdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
-					{data.article.content}
-				</Markdown>
+			{!loading ? (
+				<>
+					<Header
+						title={data.article.title}
+						description={data.article.description}
+						tags={data.article.tags}
+						createdAt={data.article.createdAt}
+						authorFirstName={data.article.author.firstname}
+						est_read_time={data.article.est_read_time}
+						authorLastName={data.article.author.lastname}
+						authorId={data.article.author._id}
+						profile_picture={data.article.author.profile_picture}
+					/>
+					<Markdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
+						{data.article.content}
+					</Markdown>
+				</>
+			) : (
+				<Spin spinning={loading} fullscreen />
 			)}
 		</main>
 	);
