05238b5fe41113fbba91bd8866dc65bce2bd162d
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-10 21:18:29 +0100

    Feature: Delete Article

--- client/src/features/readArticle/components/MenuDropDown.tsx
+++ client/src/features/readArticle/components/MenuDropDown.tsx
@@ -1,22 +1,68 @@
 import { BookOutlined, DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
-import { Dropdown } from "antd";
+import { Dropdown, message } from "antd";
 import { MoreVertOutlined } from "@mui/icons-material";
 import "./MenuDropdown.styles.sass";
 import { useAuth } from "../../../contexts/authContext";
+import { ApolloError, gql, useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
 
 interface MenuDropDownProps {
 	authorId: string;
+	articleId: string;
 }
 
-const MenuDropDown = ({ authorId }: MenuDropDownProps) => {
+const DELETE_ARTICLE_MUTATION = gql`
+	mutation DeleteArticle($deleteArticleId: String!) {
+		deleteArticle(id: $deleteArticleId) {
+			title
+		}
+	}
+`;
+
+const MenuDropDown = ({ authorId, articleId }: MenuDropDownProps) => {
 	const { currentUser } = useAuth();
-	const currentUserId = currentUser.uid ? currentUser.uid : null;
+	const currentUserId = currentUser ? currentUser.uid : null;
+	const navigate = useNavigate();
+	const [messageApi, contextHolder] = message.useMessage();
+
+	const success = (text: string) => {
+		messageApi.open({
+			type: "success",
+			content: text,
+		});
+	};
+
+	const error = (text: string) => {
+		messageApi.open({
+			type: "error",
+			content: text,
+		});
+	};
+
+	const [deleteArticle] = useMutation(DELETE_ARTICLE_MUTATION, {
+		variables: { deleteArticleId: articleId },
+		onCompleted: () => {
+			success("Article deleted successfully");
+			setTimeout(() => {
+				navigate(`/account/${currentUserId}`);
+			}, 1000);
+		},
+		onError: (err: ApolloError) => {
+			error("Error deleting article");
+			throw new Error(err.message);
+		},
+	});
 
 	const items: MenuProps["items"] = [
 		{
 			label: (
-				<button className="dropdown-item">
+				<button
+					className="dropdown-item"
+					onClick={() => {
+						if (!currentUserId) navigate("/login");
+					}}
+				>
 					<BookOutlined />
 					Save
 				</button>
@@ -44,6 +90,9 @@ const MenuDropDown = ({ authorId }: MenuDropDownProps) => {
 				<button
 					className="dropdown-item danger"
 					disabled={authorId !== currentUserId}
+					onClick={async () => {
+						await deleteArticle();
+					}}
 				>
 					<DeleteOutlined />
 					Delete
@@ -56,11 +105,14 @@ const MenuDropDown = ({ authorId }: MenuDropDownProps) => {
 	];
 
 	return (
-		<Dropdown menu={{ items }} placement="bottom" trigger={["click"]}>
-			<button type="button">
-				<MoreVertOutlined style={{ fontWeight: 700 }} />
-			</button>
-		</Dropdown>
+		<>
+			{contextHolder}
+			<Dropdown menu={{ items }} placement="bottom" trigger={["click"]}>
+				<button type="button">
+					<MoreVertOutlined style={{ fontWeight: 700 }} />
+				</button>
+			</Dropdown>
+		</>
 	);
 };
 
