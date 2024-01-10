02d54199df94688c0d3a0c88e90f182ba1f414e5
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-10 20:51:39 +0100

    Disable menu dropdown if authorId !== current user id

--- client/src/features/readArticle/components/MenuDropDown.tsx
+++ client/src/features/readArticle/components/MenuDropDown.tsx
@@ -1,53 +1,67 @@
-import React from "react";
 import { BookOutlined, DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Dropdown } from "antd";
 import { MoreVertOutlined } from "@mui/icons-material";
 import "./MenuDropdown.styles.sass";
+import { useAuth } from "../../../contexts/authContext";
 
-const items: MenuProps["items"] = [
-	{
-		label: (
-			<button className="dropdown-item">
-				<BookOutlined />
-				Save
-			</button>
-		),
-		key: "0",
-	},
-	{
-		type: "divider",
-	},
-	{
-		label: (
-			<button className="dropdown-item">
-				<EditOutlined />
-				Edit
-			</button>
-		),
-		key: "1",
-	},
-	{
-		type: "divider",
-	},
-	{
-		label: (
-			<button className="dropdown-item danger">
-				<DeleteOutlined />
-				Delete
-			</button>
-		),
-		danger: true,
-		key: "2",
-	},
-];
+interface MenuDropDownProps {
+	authorId: string;
+}
 
-const MenuDropDown: React.FC = () => (
-	<Dropdown menu={{ items }} placement="bottom" trigger={["click"]}>
-		<button type="button">
-			<MoreVertOutlined style={{ fontWeight: 700 }} />
-		</button>
-	</Dropdown>
-);
+const MenuDropDown = ({ authorId }: MenuDropDownProps) => {
+	const { currentUser } = useAuth();
+	const currentUserId = currentUser.uid ? currentUser.uid : null;
+
+	const items: MenuProps["items"] = [
+		{
+			label: (
+				<button className="dropdown-item">
+					<BookOutlined />
+					Save
+				</button>
+			),
+			key: "0",
+		},
+		{
+			type: "divider",
+		},
+		{
+			label: (
+				<button className="dropdown-item" disabled={authorId !== currentUserId}>
+					<EditOutlined />
+					Edit
+				</button>
+			),
+			key: "1",
+			disabled: authorId !== currentUserId,
+		},
+		{
+			type: "divider",
+		},
+		{
+			label: (
+				<button
+					className="dropdown-item danger"
+					disabled={authorId !== currentUserId}
+				>
+					<DeleteOutlined />
+					Delete
+				</button>
+			),
+			danger: true,
+			key: "2",
+			disabled: authorId !== currentUserId,
+		},
+	];
+
+	return (
+		<Dropdown menu={{ items }} placement="bottom" trigger={["click"]}>
+			<button type="button">
+				<MoreVertOutlined style={{ fontWeight: 700 }} />
+			</button>
+		</Dropdown>
+	);
+};
 
 export default MenuDropDown;
