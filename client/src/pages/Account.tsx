41459c729caacad1884f5228bbcf862139dcd8a3
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-11 14:00:39 +0100

    Add functionality for viewing bookmarked articles

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -3,6 +3,9 @@ import Hero from "../features/my_account/components/Hero";
 import MyArticles from "../features/my_account/components/MyArticles";
 import { gql, useQuery } from "@apollo/client";
 import Error from "../components/Error/Error";
+import { Tabs, Spin } from "antd";
+import type { TabsProps } from "antd";
+import Articles from "../components/Articles/Articles";
 
 const ACCOUNT_QUERY = gql`
 	query AccountQuery($id: String!) {
@@ -11,23 +14,64 @@ const ACCOUNT_QUERY = gql`
 			lastname
 			profile_picture
 			banner_picture
-			articles {
+			bookmarked_articles {
+				_id
 				title
 				description
-				est_read_time
 				tags
-				createdAt
+				est_read_time
 				thumbnail_url
+				createdAt
+				author {
+					firstname
+					lastname
+					profile_picture
+					_id
+				}
 			}
 		}
 	}
 `;
 
+const onChange = (key: string) => {
+	console.log(key);
+};
+
 export default function Account() {
 	const { id } = useParams();
 	const { error, loading, data } = useQuery(ACCOUNT_QUERY, {
 		variables: { id: id },
+		onCompleted(data) {
+			console.log(data);
+		},
 	});
+	const items: TabsProps["items"] = [
+		{
+			key: "1",
+			label: "My Articles",
+			children: <MyArticles id={id!} />,
+		},
+		{
+			key: "2",
+			label: "Bookmarked Articles",
+			children: loading ? (
+				<Spin />
+			) : (
+				<>
+					<h2
+						style={{
+							padding: "0px 5%",
+							margin: "50px 0px",
+							boxSizing: "border-box",
+						}}
+					>
+						Bookmarked Articles
+					</h2>
+					<Articles data={data.account.bookmarked_articles} />
+				</>
+			),
+		},
+	];
 	if (!loading) {
 		return (
 			<main className="page">
@@ -38,7 +82,13 @@ export default function Account() {
 					profile_picture={data.account.profile_picture}
 					id={id}
 				/>
-				<MyArticles id={id!} />
+				<Tabs
+					defaultActiveKey="1"
+					items={items}
+					onChange={onChange}
+					// style={{ width: "100%", paddingLeft: "7.5%" }}
+					tabBarStyle={{ width: "100%", paddingLeft: "7.5%", color: "#383230" }}
+				/>
 			</main>
 		);
 	}
