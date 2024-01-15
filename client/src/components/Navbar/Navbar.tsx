ff26d0fc613795f0ffa784513fc83de950775fbf
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-15 22:38:33 +0100

    Shut out search functionality

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -8,6 +8,8 @@ import { LoadingOutlined, MenuOutlined } from "@ant-design/icons";
 import NavAvatar from "./NavAvatar";
 import NavDropdown from "./NavDropdown";
 import MobileMenu from "./MobileMenu";
+import { FormEvent, useState } from "react";
+import { message } from "antd";
 
 const DATA = gql`
 	query getData($id: String!) {
@@ -22,6 +24,7 @@ const DATA = gql`
 
 export default function Navbar() {
 	const { currentUser } = useAuth();
+	const [messageApi, contextHolder] = message.useMessage();
 	const { data, loading } = useQuery(DATA, {
 		variables: { id: currentUser?.uid },
 		skip: !currentUser,
@@ -29,54 +32,86 @@ export default function Navbar() {
 			throw new Error(err.message);
 		},
 	});
+	const [searchText, setSearchText] = useState("");
+
+	const error = (message: string) => {
+		messageApi.open({
+			type: "error",
+			content: message,
+		});
+	};
+
+	// const success = (message: string) => {
+	// 	messageApi.open({
+	// 		type: "success",
+	// 		content: message,
+	// 	});
+	// };
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		error("This feature is still under development");
+	};
 
 	return (
-		<nav id="top-nav">
-			<Logo />
-			<form action="">
-				<label htmlFor="seachbar">
-					<SearchOutlinedIcon />
-					<input
-						type="text"
-						name="search"
-						id="searchbar"
-						placeholder="Search the platform..."
-					/>
-				</label>
-			</form>
-			{loading ? (
-				<LoadingOutlined />
-			) : data ? (
-				<NavDropdown>
-					<NavAvatar
-						firstname={data.account.firstname}
-						lastname={data.account.lastname}
-						profile_picture={data.account.profile_picture}
-					/>
-				</NavDropdown>
-			) : (
-				<div>
-					<button className="auth-nav-button" id="login-button">
-						<Link to="/login">Login</Link>
-					</button>
-					<button className="auth-nav-button" id="signup-button">
-						<Link to="/signup">Sign up</Link>
-					</button>
-				</div>
-			)}
-			{!loading && (
-				<MobileMenu currentUser={currentUser}>
-					{data ? (
+		<>
+			{contextHolder}
+			<nav id="top-nav">
+				<Logo />
+				<form
+					action=""
+					onSubmit={(e) => {
+						handleSubmit(e);
+					}}
+				>
+					<label htmlFor="seachbar">
+						<SearchOutlinedIcon />
+						<input
+							type="text"
+							name="search"
+							id="searchbar"
+							value={searchText}
+							onChange={(e) => {
+								setSearchText(e.target.value);
+							}}
+							placeholder="Search the platform..."
+						/>
+					</label>
+				</form>
+				{loading ? (
+					<LoadingOutlined />
+				) : data ? (
+					<NavDropdown>
 						<NavAvatar
 							firstname={data.account.firstname}
 							lastname={data.account.lastname}
 							profile_picture={data.account.profile_picture}
 						/>
-					) : (
-						<MenuOutlined />
-					)}
-				</MobileMenu>
-			)}
-		</nav>
+					</NavDropdown>
+				) : (
+					<div>
+						<button className="auth-nav-button" id="login-button">
+							<Link to="/login">Login</Link>
+						</button>
+						<button className="auth-nav-button" id="signup-button">
+							<Link to="/signup">Sign up</Link>
+						</button>
+					</div>
+				)}
+				{!loading && (
+					<MobileMenu currentUser={currentUser}>
+						{data ? (
+							<NavAvatar
+								firstname={data.account.firstname}
+								lastname={data.account.lastname}
+								profile_picture={data.account.profile_picture}
+							/>
+						) : (
+							<MenuOutlined />
+						)}
+					</MobileMenu>
+				)}
+			</nav>
+		</>
 	);
 }
