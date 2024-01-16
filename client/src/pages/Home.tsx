b850ae28424e87eacb7b3de31299022ed8a3af2f
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-16 18:36:36 +0100

    BugFix: Cursor doesnt show

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -34,12 +34,10 @@ export default function Home() {
 	return (
 		<div className="page" id="home-page">
 			<AnimatedCursor
-				color="#fff"
 				innerSize={8}
 				outerSize={35}
 				innerScale={1}
 				outerScale={1.7}
-				outerAlpha={0}
 				outerStyle={{
 					mixBlendMode: "exclusion",
 				}}
