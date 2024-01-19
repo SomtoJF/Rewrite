04cfd20d14ea5e67cffd4ee1f226bfa2dfed5dc1
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-19 23:22:35 +0100

    Build; uninstall react-animated-cursor

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,6 @@ import Header from "../features/home/Header";
 import "../features/home/Home.style.sass";
 import { ApolloError, gql, useQuery } from "@apollo/client";
 import Articles from "../components/Articles/Articles";
-import AnimatedCursor from "react-animated-cursor";
 
 const ARTICLES_QUERY = gql`
 	query ArticlesQuery($page: Int!) {
@@ -33,15 +32,6 @@ export default function Home() {
 
 	return (
 		<div className="page" id="home-page">
-			<AnimatedCursor
-				innerSize={8}
-				outerSize={35}
-				innerScale={1}
-				outerScale={1.7}
-				outerStyle={{
-					mixBlendMode: "exclusion",
-				}}
-			/>
 			<Header />
 			<h3 id="recent-articles">RECENT ARTICLES</h3>
 			{!loading && <Articles data={data.articles} />}
